d6769c28757cbe0b51f616431a0455f4f9e729af
juneyng <juneyng02@gmail.com>
2024-12-16 11:56:24 +0900

    [JY] FEAT: 스프링부트 프로젝트 시작

---
+++ src/test/java/com/fastcampus/springboardservice/SpringBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.springboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
