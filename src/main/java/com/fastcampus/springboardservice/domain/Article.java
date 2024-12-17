cb412368e041441b51ae2bff18556250b8cb9f19
juneyng <juneyng02@gmail.com>
2024-12-17 12:30:26 +0900

    [JY] FEAT: MySQL DB 서버와 연동 완료

--- src/main/java/com/fastcampus/springboardservice/domain/Article.java
+++ src/main/java/com/fastcampus/springboardservice/domain/Article.java
@@ -1,7 +1,20 @@
 package com.fastcampus.springboardservice.domain;
 
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
 public class Article {
     private Long id;
     private String title;
