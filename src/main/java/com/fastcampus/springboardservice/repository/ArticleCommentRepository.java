e2f4e91937cc69f97e5661520be3053825b447bc
juneyng <juneyng02@gmail.com>
2024-12-20 11:02:51 +0900

    [JY] FEAT: api로 게시글 리스트를 조회하는 테스트

--- src/main/java/com/fastcampus/springboardservice/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/springboardservice/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.fastcampus.springboardservice.repository;
 
 import com.fastcampus.springboardservice.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
 }
